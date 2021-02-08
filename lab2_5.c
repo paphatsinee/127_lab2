18684317a7be6ffd8afe922f6b72adc8bc27a8b7
Paphatsinee <paphatsinee.r@ku.th>
2021-02-08 19:01:42 +0700

    Update lab2_5.c

--- lab2_5.c
+++ lab2_5.c
@@ -6,6 +6,11 @@ int main()
     scanf("%s",n);
     for(i=0;i<n[i];i++)
     {
+        switch(n[i])
+        {
+            case '0': printf("Zero"); break;
+            case '1': printf("One"); break;
+            case '2': printf("Two");
 
     }
 
