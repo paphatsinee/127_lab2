b533b92c605cd1d9d277abec1ee661998fe4ee37
Paphatsinee <paphatsinee.r@ku.th>
2021-02-08 16:58:13 +0700

    Update lab2_1.c

--- lab2_1.c
+++ lab2_1.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
+    int a,b,c,sum;
     printf("Enter a:");
     scanf("%d",&a);
     printf("Enter b:");
@@ -10,6 +10,34 @@ int main()
     scanf("%d",&c);
     if(0<=a<=30 && 0<=b<=30 && 0<=c<=40)
     {
+        sum=a+b+c;
+        if(sum>=0&&sum<=100)
+        {
+            if(sum>=80)
+            printf("A");
 
+            else if(sum>=75)
+            printf("B+");
+
+            else if(sum>=70)
+            printf("B");
+
+            else if(sum>=65)
+            printf("C+");
+
+            else if(sum>=60)
+            printf("C");
+
+            else if(sum>=55)
+            printf("D+");
+
+            else if(sum>=50)
+            printf("D");
+
+            else
+            printf("F");
+        }
     }
+
+    return 0;
 }
