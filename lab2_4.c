040794148837f261b64c2f34c70c53fca778d348
Paphatsinee <paphatsinee.r@ku.th>
2021-02-08 18:36:50 +0700

    Create lab2_4.c

---
+++ lab2_4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int d;
+    char n,m=92;
+    scanf("%c" ,&n);
+    scanf("%d" ,&d);
+    if(d%10==5)
+    {
+        printf("%c" ,m);
+    }
+    if(d%2==1)
+    {
+
+    }
+
+    return 0;
+}
