2dd6a423182d0cd4da3dde15be6c1f01d66cccba
Paphatsinee <paphatsinee.r@ku.th>
2021-02-08 17:20:21 +0700

    Update lab2_2.c

--- lab2_2.c
+++ lab2_2.c
@@ -2,7 +2,20 @@
 int main()
 {
     char pro;
-    float T;
+    float T,M,N;
     scanf("%c",&pro);
     scanf("%f",&T);
+    if(pro=='A')
+    {
+        if(T<=200)
+        M=199;
+        else if(T>200)
+        {
+            N=T-200;
+            N=N*3;
+            M=199+N;
+        }
+
+    }
+    printf("%.2f",M);
 }
