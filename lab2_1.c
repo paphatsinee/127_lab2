35283ee5389870533412ecfd5b21e75ce920bfb4
Paphatsinee <paphatsinee.r@ku.th>
2021-02-08 16:47:51 +0700

    Update lab2_1.c

--- lab2_1.c
+++ lab2_1.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 int main()
 {
-
+    int a,b,c;
+    printf("Enter a:");
+    scanf("%d",&a);
+    printf("Enter b:");
+    scanf("%d",&b);
+    printf("Enter c:");
+    scanf("%d",&c);
 }
