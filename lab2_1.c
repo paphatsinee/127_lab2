593b530c9420cb570dedcf13691658e021c4196e
Paphatsinee <paphatsinee.r@ku.th>
2021-02-08 17:01:05 +0700

    Update lab2_1.c

--- lab2_1.c
+++ lab2_1.c
@@ -2,11 +2,8 @@
 int main()
 {
     int a,b,c,sum;
-    printf("Enter a:");
     scanf("%d",&a);
-    printf("Enter b:");
     scanf("%d",&b);
-    printf("Enter c:");
     scanf("%d",&c);
     if(0<=a<=30 && 0<=b<=30 && 0<=c<=40)
     {
