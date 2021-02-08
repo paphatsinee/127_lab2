bc76c77e5ab974b562d4cef4f15bf34ebb5d24ed
Paphatsinee <paphatsinee.r@ku.th>
2021-02-08 19:49:13 +0700

    Update lab2_3.c

--- lab2_3.c
+++ lab2_3.c
@@ -11,5 +11,34 @@ int main()
         a=b;
         b=sort;
     }
+    if(a>c)
+    {
+        sort=a;
+        a=c;
+        c=sort;
+    }
+    if(b>c)
+    {
+        sort=b;
+        b=c;
+        c=sort;
+    }
+    scanf("%s" ,d);
+    for(i=0;i<3;i++)
+    {
+        if(d[i]=='A')
+        {
+            e[i]=a;
+        }
+        else if(d[i]=='B')
+        {
+            e[i]=b;
+        }
+        else if(d[i]=='C')
+        {
+            e[i]=c;
+        }
+    }
+    printf( "%d %d %d", e[0], e[1], e[2] );
     return 0;
 }
